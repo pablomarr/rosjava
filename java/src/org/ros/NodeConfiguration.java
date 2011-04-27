3b98ff5ca6d5b348da596178fc79ee8166324b91
Damon Kohler <damonkohler@google.com>
2011-04-27 16:43:25 +0200

    Finish activity onPause().

---
+++ java/src/org/ros/NodeConfiguration.java
@@ -0,0 +1,146 @@
+/*
+ * Copyright (C) 2011 Google Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.ros;
+
+import org.ros.namespace.NameResolver;
+
+import java.net.URI;
+import java.util.List;
+
+/**
+ * Stores contextual information about a ROS node, including common ROS
+ * configuration like the master URI.
+ * 
+ * @author ethan.rublee@gmail.com (Ethan Rublee)
+ * @author kwc@willowgarage.com (Ken Conley)
+ */
+public class NodeConfiguration {
+
+  private NameResolver resolver;
+  private URI masterUri;
+  private String hostname;
+  private String rosRoot;
+  private List<String> rosPackagePath;
+  private int tcpRosPort;
+  private int xmlRpcPort;
+  private String nodeNameOverride;
+
+  /**
+   * default context
+   */
+  public NodeConfiguration() {
+    resolver = null;
+    tcpRosPort = 0;
+    xmlRpcPort = 0;
+  }
+
+  /**
+   * @return The {@link NameResolver} for a {@link Node}'s parent namespace.
+   * @see NameResolver
+   */
+  public NameResolver getParentResolver() {
+    return resolver;
+  }
+
+  public void setParentResolver(NameResolver resolver) {
+    this.resolver = resolver;
+  }
+
+  public URI getRosMasterUri() {
+    return masterUri;
+  }
+
+  public void setRosMasterUri(URI rosMasterUri) {
+    this.masterUri = rosMasterUri;
+  }
+
+  public String getRosRoot() {
+    return rosRoot;
+  }
+
+  public void setRosRoot(String rosRoot) {
+    this.rosRoot = rosRoot;
+  }
+
+  public void setRosRoot(List<String> rosPackagePath) {
+    this.rosPackagePath = rosPackagePath;
+  }
+
+  public List<String> getRosPackagePath() {
+    return rosPackagePath;
+  }
+
+  public void setRosPackagePath(List<String> rosPackagePath) {
+    this.rosPackagePath = rosPackagePath;
+  }
+
+  /**
+   * @return host name/address to use when advertising this node via URLs.
+   */
+  public String getHostName() {
+    return hostname;
+  }
+
+  /**
+   * Set host name/address to use when advertising this node via URLs.
+   * 
+   * @param hostName
+   */
+  public void setHostName(String hostName) {
+    this.hostname = hostName;
+  }
+
+  /**
+   * @return Port to bind TCPROS server to, or 0 to bind to any open port.
+   */
+  public int getTcpRosPort() {
+    return tcpRosPort;
+  }
+
+  /**
+   * Set port to bind TCPROS server to. 0 binds to any open port.
+   */
+  public void setTcpRosPort(int tcpRosPort) {
+    this.tcpRosPort = tcpRosPort;
+  }
+
+  /**
+   * @return Port to bind XMLRPC server to, or 0 to bind to any open port.
+   */
+  public int getXmlRpcPort() {
+    return xmlRpcPort;
+  }
+
+  /**
+   * Set port to bind XMLRPC server to. 0 binds to any open port.
+   */
+  public void setXmlRpcPort(int xmlRpcPort) {
+    this.xmlRpcPort = xmlRpcPort;
+  }
+
+  /**
+   * @return Override for Node name or null if no override.
+   */
+  public String getNodeNameOverride() {
+    return nodeNameOverride;
+  }
+
+  public void setNodeNameOverride(String nodeNameOverride) {
+    this.nodeNameOverride = nodeNameOverride;
+  }
+
+}
---
+++ java/src/org/ros/Subscriber.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (C) 2011 Google Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.ros;
+
+import org.ros.message.Message;
+
+/**
+ * Handles a subscription to a ROS topic.
+ * 
+ * @author ethan.rublee@gmail.com (Ethan Rublee)
+ * 
+ * @param <MessageType>
+ */
+public class Subscriber<MessageType> {
+
+  private final Class<MessageType> messageClass;
+  private final String topicName;
+
+  private final org.ros.internal.node.topic.Subscriber<MessageType> subscriber;
+  private final MessageListener<MessageType> messageListener;
+
+  protected Subscriber(String topicName, MessageListener<MessageType> messageListener,
+      Class<MessageType> messageClass,
+      org.ros.internal.node.topic.Subscriber<MessageType> subscriber) {
+    this.messageClass = messageClass;
+    this.topicName = topicName;
+    this.messageListener = messageListener;
+    this.subscriber = subscriber;
+  }
+
+  /**
+   * Cancels this subscription.
+   */
+  public void cancel() {
+    subscriber.removeMessageListener(messageListener);
+  }
+
+  /**
+   * @return The name of the subscribed topic.
+   */
+  public String getTopicName() {
+    return topicName;
+  }
+
+  /**
+   * @return the {@link Message} class literal for the subscribed topic.
+   */
+  public Class<MessageType> getTopicMessageClass() {
+    return messageClass;
+  }
+
+  /**
+   * @return The {@link MessageListener} for this {@link Subscriber}.
+   */
+  public MessageListener<MessageType> getMessageListener() {
+    return messageListener;
+  }
+
+}
