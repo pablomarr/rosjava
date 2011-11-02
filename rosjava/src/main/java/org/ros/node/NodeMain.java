52ffc70c5e759c58504a16ae1e0e616e0eb7837f
Damon Kohler <damonkohler@google.com>
2011-11-02 12:01:08 +0100

    Fix a potential race condition by making a copy of NodeConfiguration in the main NodeRunner thread. Added a template build.xml (now used in android_hokuyo). Added additional debug logging.

---
+++ rosjava/src/main/java/org/ros/node/NodeMain.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2011 Google Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.ros.node;
+
+/**
+ * Defines a program entry point and shutdown routine.
+ * 
+ * @author ethan.rublee@gmail.com (Ethan Rublee)
+ * @author damonkohler@google.com (Damon Kohler)
+ */
+public interface NodeMain {
+
+  /**
+   * Program entry point.
+   * 
+   * @param the
+   *          configured {@link Node} associated with this {@link NodeMain}
+   */
+  void main(Node node) throws Exception;
+  
+  /**
+   * Program shutdown routine.
+   */
+  void shutdown();
+  
+}
